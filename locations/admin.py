51ad736cc9f8046a1c36a56acccc0f45a71759ef
rubinsztajn <rubinsztajn@gmail.com>
2012-11-03 21:52:32 -0400

    Removing locations from admin

--- locations/admin.py
+++ locations/admin.py
@@ -1,6 +1,6 @@
 from locations.models import Room, LocationMap
 from django.contrib import admin
 
-admin.site.register(Room)
+# admin.site.register(Room)
 # Here for testing
-admin.site.register(LocationMap)
+# admin.site.register(LocationMap)
